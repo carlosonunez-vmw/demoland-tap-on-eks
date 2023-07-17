54b099a97c2494969703cb0e3f7407df8c2e3927
Carlos Nunez <ncarlos@vmware.com>
2023-07-17 14:09:48 -0500

    [fix] Create and permission the dev namespace.
    Signed-off-by: Carlos Nunez <ncarlos@vmware.com>

--- 8-install-tap.sh
+++ 8-install-tap.sh
@@ -8,7 +8,7 @@ install_tap() {
   template=$(ytt -v domain_name="$DOMAIN_NAME" \
     -v tap_version="$TAP_VERSION" \
     -v catalog_url="$GIT_CATALOGS_URL" \
-    -v app_namespace=apps \
+    -v app_namespace="$DEV_NAMESPACE" \
     -f "$(dirname "$0")/conf/values.yaml") || return 1
   tanzu package install tap \
     -p tap.tanzu.vmware.com \
