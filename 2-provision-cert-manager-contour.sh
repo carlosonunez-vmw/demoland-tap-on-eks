d04f431778d356471f93c6b64fcaaa177272257e
Carlos Nunez <ncarlos@vmware.com>
2023-08-25 16:33:57 -0500

    [fix] Prevent cert-manager EKS IRSA annotations from getting blown away
    Signed-off-by: Carlos Nunez <ncarlos@vmware.com>

--- 2-provision-cert-manager-contour.sh
+++ 2-provision-cert-manager-contour.sh
@@ -64,12 +64,31 @@ annotate_cert_manager_with_irsa_ref() {
     kubectl rollout restart -n cert-manager deployment cert-manager
 }
 
+# Since we don't have a way of adding annotations to the instance of
+# cert-manager installed by this Tanzu package, annotations are
+# added after cert-manager is installed.
+#
+# Since this isn't the desired state that kapp knew about during the install,
+# kapp undoes these changes. This prevents cert-manager from receiving the
+# AWS IAM role to request via EKS IRSA, which prevents DNS challenges
+# from succeeding and new Certificates issued by cert-manager to never
+# be issued.
+#
+# This function tells kapp to not reconcile this app so that this doesn't
+# happen.
+pause_cert_manager_kapp_reconciliation_so_annotations_remain() {
+  kubectl -n cert-manager patch sa cert-manager \
+    --type merge \
+    --patch '{"spec":{"paused":true}}'
+}
+
 region=$(docker-compose run --rm terraform output -raw aws_region) || exit 1
 iam_role=$(docker-compose run --rm terraform output -raw certmanager_role_arn) || exit 1
 domain="$(docker-compose run --rm terraform output -raw tap-domain)" || exit 1
 create_pkg_namespace  &&
   add_tanzu_standard_pkg_repo  &&
   install_cert_manager  &&
+  pause_cert_manager_kapp_reconciliation_so_annotations_remain &&
   annotate_cert_manager_with_irsa_ref "$iam_role" &&
   install_contour &&
   install_cluster_issuer "$domain" "$EMAIL_ADDRESS" "$region" &&
